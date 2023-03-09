ab0ca6edcefd78441a182bd25f3ec91e8150baae
TAE HEON KWON <PCloud63514@gmail.com>
2023-03-10 00:05:57 +0900

    test: 타입 별로 갖고와서 동작시키기
    이런 형태로 동작이 가능하니 reflect.TypeOf를 직접 쓸 이유가 줄어들었다.

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,77 @@
 package main
 
 import (
-	. "main/pkg"
+	"fmt"
 )
 
+type Foo struct {
+}
+
+func (foo *Foo) Hello() {
+	fmt.Println("Hello!")
+}
+
+func (foo *Foo) Handle() {
+	fmt.Println("Foo Handle!")
+	foo.Hello()
+}
+
+func (foo *Foo) Start() {
+	fmt.Println("Foo Start!")
+	foo.Hello()
+}
+
+type Bar struct {
+}
+
+func (bar *Bar) Start() {
+	fmt.Println("Bar Start!")
+}
+
+func (bar *Bar) Stop() {
+	fmt.Println("Bar Stop!")
+}
+
 func main() {
-	GoatApplication().Run()
+	var instances []any = []any{&Foo{}, &Bar{}}
+
+	handlers := GetInstanceByType[Handler](instances)
+	for _, handler := range handlers {
+		handler.Handle()
+	}
+
+	startEventListeners := GetInstanceByType[StartEventListener](instances)
+	for _, eventListener := range startEventListeners {
+		eventListener.Start()
+	}
+
+	stopEventListeners := GetInstanceByType[StopEventListener](instances)
+	for _, eventListener := range stopEventListeners {
+		eventListener.Stop()
+	}
+
+	//GoatApplication().Run()
+}
+
+func GetInstanceByType[T any](instances []any) []T {
+	result := []T{}
+
+	for _, instance := range instances {
+		if v, ok := instance.(T); ok {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+type Handler interface {
+	Handle()
+}
+
+type StartEventListener interface {
+	Start()
+}
+
+type StopEventListener interface {
+	Stop()
 }
