e5805290924757a102fbb8cf6dcbcd06d77cff08
TAE HEON KWON <PCloud63514@gmail.com>
2025-03-14 23:21:05 +0900

    add: testify 의존성 추가 (cache_test 수정)

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,44 +1,51 @@
 package cache
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 type TestItem struct{}
 
 func TestLRUCache_New(t *testing.T) {
 	cache := NewLRUCache[*TestItem]("Name", 10, defaultKeyFUnc)
-	if cache == nil {
-		t.Fatal("Failed to create an instance.")
-	}
+	assert.NotNil(t, cache, "Failed to create an instance.")
 }
 
 func TestLRUCache_Name(t *testing.T) {
 	cache := NewLRUCache[*TestItem]("Name", 10, defaultKeyFUnc)
-	if cache.Name() != "Name" {
-		t.Errorf("Name() = %s; want Name", cache.Name())
-	}
+	assert.Equal(t, cache.Name(), "Name", "Name() = %s; want Name", cache.Name())
 }
 
 func TestLRUCache_Get(t *testing.T) {
 	cache := NewLRUCache[*TestItem]("Name", 10, defaultKeyFUnc)
-	cache.Put([]any{"key"}, &TestItem{})
-	if _, ok := cache.Get("key"); !ok {
-		t.Errorf("Get() = (_, false); want (_, true)")
-	}
+	t.Run("should return false if there is no data", func(t *testing.T) {
+		_, ok := cache.Get("key")
+		assert.Equal(t, ok, false, "Get() = (value, ok); want (_, true)")
+	})
+
+	t.Run("should return true if there is data", func(t *testing.T) {
+		givenItem := &TestItem{}
+		cache.Put([]any{"key"}, &TestItem{})
+		v, ok := cache.Get("key")
+		assert.Equal(t, ok, true, "Get() = (_, false); want (_, true)")
+		assert.Equal(t, v, givenItem, "Get() = (_, false); want (_, true)")
+	})
 }
 
 func TestLRUCache_Put(t *testing.T) {
+	givenItem := &TestItem{}
 	cache := NewLRUCache[*TestItem]("Name", 10, defaultKeyFUnc)
 	cache.Put([]any{"key"}, &TestItem{})
-	if _, ok := cache.Get("key"); !ok {
-		t.Errorf("Put() = (_, false); want (_, true)")
-	}
+	v, ok := cache.Get("key")
+	assert.Equal(t, ok, true, "Put() = (_, false); want (_, true)")
+	assert.Equal(t, v, givenItem, "Put() = (_, false); want (_, true)")
 }
 
 func TestLRUCache_Delete(t *testing.T) {
 	cache := NewLRUCache[*TestItem]("Name", 10, defaultKeyFUnc)
 	cache.Put([]any{"key"}, &TestItem{})
 	cache.Delete("key")
-	if _, ok := cache.Get("key"); ok {
-		t.Errorf("Delete() = (_, true); want (_, false)")
-	}
+	_, ok := cache.Get("key")
+	assert.Equal(t, ok, false, "Delete() = (_, false); want (_, true)")
 }
