7e0b25a3536211728b1927f1cd6557b5aba524f9
TAE HEON KWON <PCloud63514@gmail.com>
2025-03-15 20:42:47 +0900

    add: ttl 지원 캐시 추가

---
+++ cache/expire.go
@@ -0,0 +1,124 @@
+package cache
+
+import (
+	"container/list"
+	"sync"
+	"time"
+)
+
+type expireCache[T any] struct {
+	name            string
+	mu              sync.RWMutex
+	cache           map[string]*list.Element
+	ll              *list.List
+	cacheSize       int
+	keyFunc         KeyFunc
+	ttl             time.Duration
+	expireExtension bool
+}
+
+type expireCacheItem[T any] struct {
+	key        string
+	value      T
+	expiration time.Time
+}
+
+func NewExpireCache[T any](name string, capacity int, keyFunc KeyFunc, ttl time.Duration, expireExtension bool) Cache[T] {
+	return &expireCache[T]{
+		name:            name,
+		mu:              sync.RWMutex{},
+		cache:           make(map[string]*list.Element),
+		ll:              list.New(),
+		cacheSize:       capacity,
+		keyFunc:         keyFunc,
+		ttl:             ttl,
+		expireExtension: expireExtension,
+	}
+}
+
+func (ex *expireCache[T]) Name() string {
+	return ex.name
+}
+
+func (ex *expireCache[T]) Get(keys ...any) (T, bool) {
+	key := ex.keyFunc(keys...)
+	return ex.get(key)
+}
+
+func (ex *expireCache[T]) Put(keys []any, value T) {
+	key := ex.keyFunc(keys...)
+	ex.put(key, value)
+}
+
+func (ex *expireCache[T]) Delete(keys ...any) {
+	key := ex.keyFunc(keys...)
+	ex.delete(key)
+}
+
+func (ex *expireCache[T]) get(key string) (T, bool) {
+	ex.mu.RLock()
+	var zeroValue T
+	if elem, found := ex.cache[key]; found {
+		ex.ll.MoveToFront(elem)
+		item := elem.Value.(*expireCacheItem[T])
+		if item.isExpired() {
+			ex.mu.RUnlock()
+			ex.mu.Lock()
+			defer ex.mu.Unlock()
+			ex.ll.Remove(elem)
+			delete(ex.cache, key)
+			return zeroValue, false
+		}
+		ex.mu.RUnlock()
+		if ex.expireExtension {
+			ex.mu.Lock()
+			defer ex.mu.Unlock()
+			item.expiration = time.Now().Add(ex.ttl)
+		}
+		return item.value, true
+	}
+	defer ex.mu.RUnlock()
+	return zeroValue, false
+}
+
+func (ex *expireCache[T]) put(key string, value T) {
+	ex.mu.Lock()
+	defer ex.mu.Unlock()
+	expiration := time.Now().Add(ex.ttl)
+	if elem, found := ex.cache[key]; found {
+		ex.ll.MoveToFront(elem)
+		item := elem.Value.(*expireCacheItem[T])
+		item.value = value
+		item.expiration = expiration
+		return
+	}
+
+	elem := ex.ll.PushFront(&expireCacheItem[T]{key, value, expiration})
+	ex.cache[key] = elem
+
+	if ex.ll.Len() > ex.cacheSize {
+		last := ex.ll.Back()
+		if last != nil {
+			ex.ll.Remove(last)
+			delete(ex.cache, last.Value.(*expireCacheItem[T]).key)
+		}
+	}
+}
+
+func (ex *expireCache[T]) delete(key string) {
+	ex.mu.Lock()
+	defer ex.mu.Unlock()
+
+	if elem, found := ex.cache[key]; found {
+		ex.ll.Remove(elem)
+		delete(ex.cache, key)
+	}
+}
+
+func (ei *expireCacheItem[T]) isExpired() bool {
+	return time.Now().After(ei.expiration)
+}
+
+func (ei *expireCacheItem[T]) isNotExpired() bool {
+	return !time.Now().After(ei.expiration)
+}
---
+++ cache/expire_test.go
@@ -0,0 +1,48 @@
+package cache
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+type TestExpireCacheItem struct{}
+
+func TestExpireCache_New(t *testing.T) {
+	cache := NewExpireCache[*TestExpireCacheItem]("Name", 10, defaultKeyFunc, time.Second*3, false)
+	assert.NotNil(t, cache, "Failed to create an instance.")
+}
+
+func TestExpireCache_Name(t *testing.T) {
+	cache := NewExpireCache[*TestExpireCacheItem]("Name", 10, defaultKeyFunc, time.Second*3, false)
+	assert.Equal(t, cache.Name(), "Name", "Name() = %s; want Name", cache.Name())
+}
+
+func TestExpireCache_Get(t *testing.T) {
+	cache := NewExpireCache[*TestExpireCacheItem]("Name", 10, defaultKeyFunc, time.Second*1, false)
+
+	t.Run("should return false if there is no data", func(t *testing.T) {
+		_, ok := cache.Get("key")
+		assert.Equal(t, ok, false, "Get() = (_, false); want (_, true)")
+	})
+
+	t.Run("should return true if there is data", func(t *testing.T) {
+		cache.Put([]any{"key"}, &TestExpireCacheItem{})
+		_, ok := cache.Get("key")
+		assert.Equal(t, ok, true, "Get() = (_, false); want (_, true)")
+	})
+
+	t.Run("should return false if data is expired", func(t *testing.T) {
+		cache.Put([]any{"key"}, &TestExpireCacheItem{})
+		time.Sleep(time.Second * 2)
+		_, ok := cache.Get("key")
+		assert.Equal(t, ok, false, "Get() = (_, false); want (_, true)")
+	})
+}
+
+func TestExpireCache_Put(t *testing.T) {
+	cache := NewExpireCache[*TestExpireCacheItem]("Name", 10, defaultKeyFunc, time.Second*1, false)
+	cache.Put([]any{"key"}, &TestExpireCacheItem{})
+	_, ok := cache.Get("key")
+	assert.Equal(t, ok, true, "Put() = (_, false); want (_, true)")
+}
