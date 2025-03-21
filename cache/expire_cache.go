0df3b58f888147a8eb264e6d739f60460fd4b572
TAE HEON KWON <PCloud63514@gmail.com>
2025-03-21 21:12:54 +0900

    fix: expire_cache cache_store 적용

--- cache/expire_cache.go
+++ cache/expire_cache.go
@@ -1,21 +1,14 @@
 package cache
 
-import (
-	"container/list"
-	"sync"
-	"time"
-)
+import "time"
 
-type expireCache[T any] struct {
+type ExpireCache[T any] struct {
 	name            string
-	mu              sync.RWMutex
-	cache           map[string]*list.Element
-	ll              *list.List
-	cacheSize       int
+	store           *cacheStore[T, *expireCacheItem[T]]
 	keyGen          *keyGenerator
+	metrics         *cacheMetrics
 	ttl             time.Duration
 	expireExtension bool
-	metrics         *cacheMetrics
 }
 
 type expireCacheItem[T any] struct {
@@ -25,116 +18,89 @@ type expireCacheItem[T any] struct {
 }
 
 func NewExpireCache[T any](name string, capacity int, ttl time.Duration, expireExtension bool) Cache[T] {
-	return &expireCache[T]{
+	return &ExpireCache[T]{
 		name:            name,
-		mu:              sync.RWMutex{},
-		cache:           make(map[string]*list.Element),
-		ll:              list.New(),
-		cacheSize:       capacity,
+		store:           newCacheStore[T, *expireCacheItem[T]](capacity),
 		keyGen:          &keyGenerator{},
+		metrics:         &cacheMetrics{},
 		ttl:             ttl,
 		expireExtension: expireExtension,
-		metrics:         &cacheMetrics{},
 	}
 }
 
-func (ex *expireCache[T]) Name() string {
-	return ex.name
+func (c *ExpireCache[T]) Name() string {
+	return c.name
 }
 
-func (ex *expireCache[T]) Get(keys ...any) (T, bool) {
-	key := ex.keyGen.Generate(keys...)
-	return ex.get(key)
+func (c *ExpireCache[T]) Get(keys ...any) (T, bool) {
+	key := c.keyGen.Generate(keys)
+	return c.get(key)
 }
 
-func (ex *expireCache[T]) Put(keys []any, value T) {
-	key := ex.keyGen.Generate(keys...)
-	ex.put(key, value)
+func (c *ExpireCache[T]) Put(keys []any, value T) {
+	key := c.keyGen.Generate(keys)
+	c.put(key, value)
 }
 
-func (ex *expireCache[T]) Delete(keys ...any) {
-	key := ex.keyGen.Generate(keys...)
-	ex.delete(key)
+func (c *ExpireCache[T]) Delete(keys ...any) {
+	key := c.keyGen.Generate(keys)
+	c.delete(key)
 }
 
-func (ex *expireCache[T]) get(key string) (T, bool) {
-	ex.mu.RLock()
-	var zeroValue T
-	if elem, found := ex.cache[key]; found {
-		ex.ll.MoveToFront(elem)
-		item := elem.Value.(*expireCacheItem[T])
-		if item.isExpired() {
-			ex.mu.RUnlock()
-			ex.mu.Lock()
-			defer ex.mu.Unlock()
-			ex.ll.Remove(elem)
-			delete(ex.cache, key)
-			ex.metrics.Miss()
+func (c *ExpireCache[T]) Stat() *CacheStat {
+	return &CacheStat{
+		Name:        c.name,
+		MaxEntries:  c.store.Capacity(),
+		CurrentSize: c.store.Size(),
+		HitCount:    c.metrics.HitCount(),
+		MissCount:   c.metrics.MissCount(),
+		HitRate:     c.metrics.HitRate(),
+	}
+}
+
+func (c *ExpireCache[T]) get(key string) (T, bool) {
+	if it, ok := c.store.Get(key); ok {
+		if it.Expired() {
+			c.store.Delete(key)
+			c.metrics.Miss()
+			var zeroValue T
 			return zeroValue, false
 		}
-		ex.mu.RUnlock()
-		if ex.expireExtension {
-			ex.mu.Lock()
-			defer ex.mu.Unlock()
-			item.expiration = time.Now().Add(ex.ttl)
+		if c.expireExtension {
+			it.ExtendExpiration(c.ttl)
 		}
-		ex.metrics.Hit()
-		return item.value, true
+		c.metrics.Hit()
+		return it.Value(), true
 	}
-	ex.metrics.Miss()
-	defer ex.mu.RUnlock()
+	c.metrics.Miss()
+	var zeroValue T
 	return zeroValue, false
 }
 
-func (ex *expireCache[T]) put(key string, value T) {
-	ex.mu.Lock()
-	defer ex.mu.Unlock()
-	expiration := time.Now().Add(ex.ttl)
-	if elem, found := ex.cache[key]; found {
-		ex.ll.MoveToFront(elem)
-		item := elem.Value.(*expireCacheItem[T])
-		item.value = value
-		item.expiration = expiration
-		return
-	}
-
-	elem := ex.ll.PushFront(&expireCacheItem[T]{key, value, expiration})
-	ex.cache[key] = elem
-
-	if ex.ll.Len() > ex.cacheSize {
-		last := ex.ll.Back()
-		if last != nil {
-			ex.ll.Remove(last)
-			delete(ex.cache, last.Value.(*expireCacheItem[T]).key)
-		}
-	}
+func (c *ExpireCache[T]) put(key string, value T) {
+	c.store.Put(&expireCacheItem[T]{key: key, value: value, expiration: time.Now().Add(c.ttl)})
 }
 
-func (ex *expireCache[T]) delete(key string) {
-	ex.mu.Lock()
-	defer ex.mu.Unlock()
-
-	if elem, found := ex.cache[key]; found {
-		ex.ll.Remove(elem)
-		delete(ex.cache, key)
-	}
+func (c *ExpireCache[T]) delete(key string) {
+	c.store.Delete(key)
 }
 
-func (ei *expireCacheItem[T]) isExpired() bool {
+func (ei *expireCacheItem[T]) Key() string {
+	return ei.key
+}
+
+func (ei *expireCacheItem[T]) Value() T {
+	return ei.value
+}
+
+func (ei *expireCacheItem[T]) Expired() bool {
 	return time.Now().After(ei.expiration)
 }
 
-func (ei *expireCacheItem[T]) isNotExpired() bool {
+func (ei *expireCacheItem[T]) IsNotExpired() bool {
 	return !time.Now().After(ei.expiration)
 }
 
-func (ex *expireCache[T]) Stat() *CacheStat {
-	return &CacheStat{
-		Name:        ex.name,
-		MaxEntries:  ex.cacheSize,
-		CurrentSize: ex.ll.Len(),
-		HitCount:    ex.metrics.HitCount(),
-		MissCount:   ex.metrics.MissCount(),
-		HitRate:     ex.metrics.HitRate(),
-	}
+func (ei *expireCacheItem[T]) ExtendExpiration(ttl time.Duration) {
+	ei.expiration = time.Now().Add(ttl)
 }
